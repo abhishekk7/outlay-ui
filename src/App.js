bdf28c2a6fd9e6af61fca2e613c8338a6ff0c254
Abhishek Kumar <akumar@esi-estech.com>
2019-03-22 19:20:37 -0500

    add new page layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavigationTop from "./components/NavigationTop";
 import Routes from "./components/Routes";
+import Loader from "react-loader-spinner";
 
 class App extends Component {
   render() {
@@ -8,6 +9,12 @@ class App extends Component {
       <div>
         <NavigationTop />
         <Routes />
+        <Loader 
+         type="Puff"
+         color="#00BFFF"
+         height="100"	
+         width="100"
+      /> 
       </div>
     );
   }
--- src/components/pages/Transactions.js
+++ src/components/pages/Transactions.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+import axios from "axios";
+import { from } from "rxjs";
 
-const TransactionsPage = () => {
-  return <div>Transactions Page</div>;
-};
+class TransactionsPage extends Component {
+  state = {
+    transactions: []
+  };
 
+  componentDidMount() {
+    axios({
+      method: "post",
+      url: "http://localhost:9000/api/v1/plaid/transactions",
+      data: {
+        email: "abhishek@test.com"
+      }
+    }).then(res => {
+      const transactions = res.data.transactions;
+      this.setState({ transactions });
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Table striped bordered hover className="my-4">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.transactions.map(transaction => (
+              <tr
+                key={transaction.transaction_id}
+                className={transaction.amount < 0 ? "bg-info" : ""}
+              >
+                <td>{transaction.date}</td>
+                <td>{transaction.name}</td>
+                <td>{transaction.transaction_type}</td>
+                <td>{transaction.amount + transaction.iso_currency_code}</td>
+                <td>{transaction.category.map(value => (
+                    <li key={value}>{value}</li>
+                  ))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
 export default TransactionsPage;
